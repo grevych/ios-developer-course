647b377c8a19c1f6243de46a2661dfbeb4bf6643
Gerardo Reyes <grevych@gmail.com>
2024-04-27 20:33:10 +0200

    chore: create xcode project

---
+++ App/Course App/Course App/Course_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_AppApp.swift
+//  Course App
+//
+//  Created by Gerardo Reyes on 27.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Course_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
